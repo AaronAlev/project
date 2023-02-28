dfe0779a8c15fb0c5220bbc2b4682b881d56b88c
Aaron <aaronalev@gmail.com>
2023-02-28 13:22:39 +0200

    Mina kui kasutaja näen toote kogust ja hinda [Delivers #184526157]

--- ostukalkulaator.py
+++ ostukalkulaator.py
@@ -1,8 +1,6 @@
-
 # Tax rates
-EE = 0.2 # Estonia
-FI = 0.24 # Finland
-
+EE = 0.2  # Estonia
+FI = 0.24  # Finland
 
 amount = input("Insert product amount: ")
 price = input("Input product price: ")
